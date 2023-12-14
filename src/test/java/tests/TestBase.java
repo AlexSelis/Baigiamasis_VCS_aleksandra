0d1862fbf69d58648aa58c1aff30390809c1bf5b
AlexSelis <aleksandra.selisceva@gmail.com>
2023-12-14 18:46:03 +0200

    Add Before and After methods to TestBase class

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,15 @@
 package tests;
 
-public class TestBase {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.Common;
+
+public abstract class TestBase {
+    @BeforeMethod
+    public abstract void setUp();
+
+    @AfterMethod
+    public void tearDown(){
+        Common.quitDriver();
+    }
 }
