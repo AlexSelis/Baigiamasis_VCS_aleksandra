0e5cc82f05998bf0be9949b246d41b123cf3df82
AlexSelis <aleksandra.selisceva@gmail.com>
2023-12-14 18:45:17 +0200

    Add methods to SimkaMainPage class

--- src/test/java/pages/Simka/SimkaMainPage.java
+++ src/test/java/pages/Simka/SimkaMainPage.java
@@ -1,10 +1,31 @@
 package pages.Simka;
 
 import pages.Common;
+import pages.Locator;
 
 public class SimkaMainPage {
-    public static void openUrl(String url){
+    public static void openUrl(String url) {
         Common.setUpChrome();
         Common.openUrl(url);
+    }
+
+    public static void clickOnLoginButton() {
+        Common.clickElement(Locator.Simka.MainPage.loginButton);
+    }
+
+    public static void inputUserEmail(String userEmail) {
+        Common.sendKeysToElement(userEmail, Locator.Simka.MainPage.emailInput);
+    }
+
+    public static void inputUserPassword(String userPassword) {
+        Common.sendKeysToElement(userPassword, Locator.Simka.MainPage.passwordInput);
+    }
+
+    public static void clickOnConfirmationButton() {
+        Common.clickElement(Locator.Simka.MainPage.submitLogInButton);
+    }
+
+    public static String readElementNameAfterLogin() {
+        return Common.getElementText(Locator.Simka.MainPage.elementTextAfterSuccessfulLogin);
     }
 }
